d27d21b38df393418c1e89c321e6c0afe79932cf
Sparsh Sanchorawala <sparshno.1@gmail.com>
2017-03-04 03:11:00 +0530

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/25189334

---
+++ codeforces/4/A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		if(n % 2 == 0 && n > 2)
+		    System.out.println("YES");
+		else
+		    System.out.println("NO");
+		
+	}
+
+}
