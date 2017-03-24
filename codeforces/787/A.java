7f7cbc5967ce056f78a7f0b716b48cb0b5883926
Sparsh Sanchorawala <sparshno.1@gmail.com>
2017-03-24 18:44:00 +0530

    Add solution for problem `A - The Monster` Link: https://codeforces.com/contest/787/problem/A Tags: brute force, math, number theory, *1200 Ref: https://codeforces.com/contest/787/submission/25773867

---
+++ codeforces/787/A.java
@@ -0,0 +1,247 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+import static java.lang.Math.*;
+ 
+public class Check2 implements Runnable
+{
+	static class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+	
+    public static int gcd (int a, int b) {
+    	
+    	if(b == 0)
+    		return a;
+    	
+    	return(gcd(b, a % b));
+    }
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new Check2(),"CHECK2",1<<26).start();
+	}
+ 
+	public void run() 
+	{
+
+		InputReader s=new InputReader(System.in);
+		PrintWriter w=new PrintWriter(System.out);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		int d = s.nextInt();
+		int ans;
+		
+		int div;
+		
+		if(a > c)
+		    div = gcd(a,c);
+		else
+		    div = gcd(c, a);
+		
+		int x = b - d;
+		if(x < 0)
+		    x = -x;
+		    
+		if (x % div != 0)
+			ans = -1;
+	    else {
+	        
+	        int m = 0;
+	        
+	        while(true) {
+	            
+	            if((d - b + c * m) % a == 0 && (d + c * m >= b)) {
+	                
+	                ans = d + c * m;
+	                break;
+	            }
+	            
+	            m++;
+	                
+	        }
+	    }
+			
+			
+		w.println(ans);	
+		w.close();
+	}
+}
