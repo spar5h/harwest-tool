692feb1a47bf64a0c4ad5863e16f09bda9765fb3
Sparsh Sanchorawala <sparshno.1@gmail.com>
2017-03-24 19:14:00 +0530

    Add solution for problem `B - Not Afraid` Link: https://codeforces.com/contest/787/problem/B Tags: greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/787/submission/25774647

---
+++ codeforces/787/B.java
@@ -0,0 +1,248 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+import static java.lang.Math.*;
+ 
+public class Check2 implements Runnable
+{
+	static class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+	
+    public static int gcd (int a, int b) {
+    	
+    	if(b == 0)
+    		return a;
+    	
+    	return(gcd(b, a % b));
+    }
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new Check2(),"CHECK2",1<<26).start();
+	}
+ 
+	public void run() 
+	{
+
+		InputReader s=new InputReader(System.in);
+		PrintWriter w=new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+	    int m = s.nextInt();
+	  
+	    boolean boo = false;
+	  
+	    for(int i = 0; i < m; i++) {
+	        
+	        int[] a = new int[n + 1];
+	        int[] b = new int[n + 1];
+	        int k = s.nextInt();
+	        int count = 0;
+	        
+	        for(int j = 0; j < k; j++) {
+	            
+	            int x = s.nextInt();
+	            
+	            if(x < 0) {
+	                b[-x]++;
+	                x = -x;
+	            } 
+	            else
+	                a[x]++;
+	            
+	            
+	            
+	            if(a[x] >= 1 && b[x] >= 1)
+	                count++;
+	        }
+	        
+	        if(count == 0)
+	            boo = true;
+	    }
+			
+		if(boo == true)	
+		    w.println("YES");
+		else
+		    w.println("NO");
+		    
+		w.close();    
+	}
+	
+}
