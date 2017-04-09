d651d8586d026806423667d38a49eda56b3a6832
Sparsh Sanchorawala <sparshno.1@gmail.com>
2017-04-09 23:45:00 +0530

    Add solution for problem `A - Lesha and array splitting` Link: https://codeforces.com/contest/754/problem/A Tags: constructive algorithms, greedy, implementation, *1200 Ref: https://codeforces.com/contest/754/submission/26242773

---
+++ codeforces/754/A.java
@@ -0,0 +1,255 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+ public class codeforces implements Runnable {
+    
+	static class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new codeforces(),"codeforces",1<<26).start();
+	}
+	
+	public void run() {
+	    
+	    InputReader s = new InputReader(System.in);
+	    PrintWriter w = new PrintWriter(System.out);
+	    
+	    int n = s.nextInt();
+	    
+	    int a[] = new int[n];
+	    
+	    boolean b = false;
+	    int sum = 0;
+	    for(int i = 0; i < n; i++) {
+	    	
+	    	a[i] = s.nextInt();
+	    	
+	    	if(a[i] != 0)
+	    		b = true;
+	    	
+	    	sum += a[i];
+	    }
+	    
+	    if(b) {
+	    	
+	    	w.println("YES");
+	    	
+	    	int count = 1;
+	    	int[] l =  new int[2];
+	    	int[] r = new int[2];
+	    	l[0] = 1;
+	    	r[0] = n;
+	    	
+	    	
+	    	if(sum == 0) {
+	    		
+	    		for(int i = 0; i < n; i++) {
+	    			
+	    			if(a[i] != 0) {
+	    				l[1] = i + 2;
+	    				r[1] = n;
+	    				r[0] = i + 1;
+	    				count++;
+	    				break;
+	    			}	
+	    		
+	    		}
+	    	}
+	    	
+	    	w.println(count);
+	    	
+	    	for(int i = 0; i < count; i++) {
+	    		
+	    		w.println(l[i] + " " + r[i]);
+	    	}
+	    	
+	   }
+	    
+	    else {
+	    	
+	    	w.println("NO");
+	    }
+	    
+	    
+	    w.close();
+	}
+}
