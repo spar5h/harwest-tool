fceabe1a6675f9b27e9b82284943fb86abc7014e
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-04-12 16:15:00 +0530

    Add solution for problem `D - Full Binary Tree Queries` Link: https://codeforces.com/contest/960/problem/D Tags: brute force, implementation, trees, *2100 Ref: https://codeforces.com/contest/960/submission/37245909

---
+++ codeforces/960/D.java
@@ -0,0 +1,261 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+ 
+/* spar5h */
+ 
+public class cf5 implements Runnable{    
+	
+	static int log2 (long x) {
+		
+		int c = 0;
+		
+		while(x > 0) {
+			x /= 2; c++;
+		}
+		
+		return c;
+	}
+	
+	public void run() {
+	    
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		long[] pow2 = new long[65]; pow2[0] = 1;
+		
+		for(int i = 1; i <= 64; i++)
+			pow2[i] = pow2[i - 1] * 2;
+		
+		long[] shiftNode = new long[65];
+		long[] shiftVal = new long[65];
+		
+		int q = s.nextInt();
+		
+		while(q-- > 0) {
+			
+			int key = s.nextInt();
+			long x = s.nextLong();
+			int h = log2(x) - 1;
+			
+			if(key == 3) {
+				
+				w.print(x + " ");
+				x = (x + shiftVal[h] + pow2[62]) % pow2[h] + pow2[h];
+				x = (x + shiftNode[h] + pow2[62]) % pow2[h] + pow2[h];
+				x = x / 2;
+				h--;
+				
+				while(h >= 0) {
+					
+					long val = (x - shiftVal[h] + pow2[62]) % pow2[h] + pow2[h];
+					w.print(val + " ");
+					x = (x + shiftNode[h] + pow2[62]) % pow2[h] + pow2[h];
+					x = x / 2;
+					h--;
+				}
+				
+				w.println();
+			}
+			
+			else if(key == 1) {
+				
+				long k = s.nextLong();
+				shiftVal[h] = (shiftVal[h] + k + pow2[62]) % pow2[h];
+			}
+			
+			else if(key == 2) {
+				
+				long k = s.nextLong();
+				shiftNode[h] = (shiftNode[h] + k + pow2[62]) % pow2[h];
+			}
+	
+		}
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new cf5(),"cf5",1<<26).start();
+	}
+}   
