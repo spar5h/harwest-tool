0babdee73c406888670c5c4f15475aaeeefabc4d
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-06-26 16:57:00 +0530

    Add solution for problem `B - Good Sequences` Link: https://codeforces.com/contest/264/problem/B Tags: dp, number theory, *1500 Ref: https://codeforces.com/contest/264/submission/39673969

---
+++ codeforces/264/B.java
@@ -0,0 +1,242 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+ 
+/* spar5h */
+
+public class cf4 implements Runnable{    
+	
+	final static long mod = (long)1e9 + 7; 
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		
+		for(int i = 1; i <= n; i++) {
+			
+			int x = s.nextInt();
+			
+			ArrayList<Integer> list = new ArrayList<Integer>(); list.add(x);
+			
+			int max = 0;
+			
+			for(int j = 2; j * j <= x; j++) {
+				
+				if(x % j != 0)
+					continue;
+				
+				list.add(j);
+				
+				if(j != x / j)
+					list.add(x / j);
+				
+				if(hm.get(j) != null)
+					max = Math.max(max, hm.get(j));
+				
+				if(hm.get(x / j) != null)
+					max = Math.max(max, hm.get(x / j));
+			}
+			
+			for(int j = 0; j < list.size(); j++)
+				hm.put(list.get(j), max + 1);
+		}
+		
+		int res = 0;
+		
+		for(int i : hm.values())
+			res = Math.max(i, res);
+		
+		w.println(res);
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return str;
+	}
+	public int nextInt()
+	{
+		int c = read();
+		
+		while(isSpaceChar(c)) 
+			c = read();
+		
+		int sgn = 1;
+		
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		
+		int res = 0;
+		do 
+		{
+			if(c<'0'||c>'9') 
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c)); 
+		
+		return res * sgn;
+	}
+	
+	public long nextLong() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		
+		do 
+		{
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c));
+			return res * sgn;
+	}
+	
+	public double nextDouble() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		double res = 0;
+		while (!isSpaceChar(c) && c != '.') 
+		{
+			if (c == 'e' || c == 'E')
+				return res * Math.pow(10, nextInt());
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		if (c == '.') 
+		{
+			c = read();
+			double m = 1;
+			while (!isSpaceChar(c)) 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				m /= 10;
+				res += (c - '0') * m;
+				c = read();
+			}
+		}
+		return res * sgn;
+	}
+	
+	public String readString() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do 
+		{
+			res.appendCodePoint(c);
+			c = read();
+		} 
+		while (!isSpaceChar(c));
+		
+		return res.toString();
+	}
+ 
+	public boolean isSpaceChar(int c) 
+	{
+		if (filter != null)
+			return filter.isSpaceChar(c);
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+ 
+	public String next() 
+	{
+		return readString();
+	}
+	
+	public interface SpaceCharFilter 
+	{
+		public boolean isSpaceChar(int ch);
+	}
+}
+
+public static void main(String args[]) throws Exception
+{
+	new Thread(null, new cf4(),"cf4",1<<26).start();
+	}
+}   
