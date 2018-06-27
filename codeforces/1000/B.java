d0e0d1cf545731ff12725cdffc94f8ad9594572a
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-06-27 21:04:00 +0530

    Add solution for problem `B - Light It Up` Link: https://codeforces.com/contest/1000/problem/B Tags: greedy, *1500 Ref: https://codeforces.com/contest/1000/submission/39713882

---
+++ codeforces/1000/B.java
@@ -0,0 +1,276 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+ 
+/* spar5h */ 
+
+public class cf2 implements Runnable{    
+	
+	final static long mod = (long)1e9 + 7; 
+	
+	public void run() {
+	    
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		int m = s.nextInt();
+		
+		long[] a = new long[n + 1];
+		
+		for(int i = 1; i <= n; i++)
+			a[i] = s.nextInt();
+		
+		long[] sum = new long[n + 1];
+		
+		for(int i = 0; i <= n; i+=2) {
+			
+			if(i != 0)
+				sum[i] = sum[i - 1];
+			
+			if(i == n)
+				sum[i] += m - a[i];
+			
+			else
+				sum[i] += a[i + 1] - a[i];
+			
+			if(i + 1 <= n)
+				sum[i + 1] = sum[i];
+		}
+		
+		long res = sum[n];
+		
+		for(int i = 0; i <= n; i++) {
+			
+			if(i == n && m - a[i] == 1)
+				continue;
+			
+			if(i != n && a[i + 1] - a[i] == 1)
+				continue;
+			
+			if(i % 2 == 0) {
+				
+				if(i == n)
+					continue;
+				
+				long x = sum[n] - sum[i];				
+				x = (m - a[i + 1]) - x;
+				
+				x += a[i + 1] - 1 - a[i];
+				
+				if(i != 0)
+					x += sum[i - 1];
+				
+				res = Math.max(x, res);
+			}
+			
+			else {
+				
+				if(i == n) {
+					long x = sum[n] + m - (a[i] + 1); res = Math.max(x, res); continue;
+				}
+				
+				long x = sum[n] - sum[i];
+				x = (m - a[i + 1]) - x;
+				
+				x += a[i + 1] - (a[i] + 1);
+				
+				x += sum[i - 1];
+				
+				res = Math.max(x, res);
+				
+			}
+		
+		}
+		
+		w.println(res);
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new cf2(),"cf2",1<<26).start();
+	}
+}   
+
