0f1da9eff0cc475f0ff1678e69eba8d6c4779a44
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-07-31 20:57:00 +0530

    Add solution for problem `D - Walking Between Houses` Link: https://codeforces.com/contest/1015/problem/D Tags: constructive algorithms, greedy, *1600 Ref: https://codeforces.com/contest/1015/submission/41041824

---
+++ codeforces/1015/D.java
@@ -0,0 +1,260 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.awt.List;
+import java.io.*;
+ 
+/* spar5h */
+
+public class cf4 implements Runnable{    
+
+	final static long mod = (long)1e9 + 7; 
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int t = 1;
+		
+		while(t-- > 0) {
+			
+			long n = s.nextInt(), k = s.nextInt();
+			long m = s.nextLong();
+			
+			if(k * (n - 1) < m || m < k) {
+				w.println("NO"); continue;
+			}
+			
+			ArrayList<Long> list = new ArrayList<Long>();
+			
+			long pos = 1;
+			
+			while(k > 0 && m - (n - 1) >= k - 1) {
+				m -= n - 1; k--; pos = n + 1 - pos; list.add(pos);
+			}
+			
+			int add = 0;
+			
+			if(pos == 1) {
+				
+				if(m > k) {
+					pos += m - (k - 1); m -= m - (k - 1); k--; list.add(pos);
+				}
+				
+				add = 1;
+			}
+			
+			else {
+				
+				if(m > k) {
+					pos -= m - (k - 1); m -= m - (k - 1); k--; list.add(pos);
+				}
+				
+				add = -1;
+			}
+			
+			while(k > 0) {
+				
+				if(pos == n && add == 1)
+					add = -1;
+				else if(pos == 1 && add == -1)
+					add = 1;
+				
+				pos += add; k--; list.add(pos);
+			}
+			
+			w.println("YES");
+			
+			for(int i = 0; i < list.size(); i++)
+				w.print(list.get(i) + " ");
+			
+			w.println();
+		}
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return str;
+	}
+	public int nextInt()
+	{
+		int c = read();
+		
+		while(isSpaceChar(c)) 
+			c = read();
+		
+		int sgn = 1;
+		
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		
+		int res = 0;
+		do 
+		{
+			if(c<'0'||c>'9') 
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c)); 
+		
+		return res * sgn;
+	}
+	
+	public long nextLong() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		
+		do 
+		{
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c));
+			return res * sgn;
+	}
+	
+	public double nextDouble() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		double res = 0;
+		while (!isSpaceChar(c) && c != '.') 
+		{
+			if (c == 'e' || c == 'E')
+				return res * Math.pow(10, nextInt());
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		if (c == '.') 
+		{
+			c = read();
+			double m = 1;
+			while (!isSpaceChar(c)) 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				m /= 10;
+				res += (c - '0') * m;
+				c = read();
+			}
+		}
+		return res * sgn;
+	}
+	
+	public String readString() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do 
+		{
+			res.appendCodePoint(c);
+			c = read();
+		} 
+		while (!isSpaceChar(c));
+		
+		return res.toString();
+	}
+ 
+	public boolean isSpaceChar(int c) 
+	{
+		if (filter != null)
+			return filter.isSpaceChar(c);
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+ 
+	public String next() 
+	{
+		return readString();
+	}
+	
+	public interface SpaceCharFilter 
+	{
+		public boolean isSpaceChar(int ch);
+	}
+}
+
+public static void main(String args[]) throws Exception
+{
+	new Thread(null, new cf4(),"cf4",1<<26).start();
+	}
+}   
