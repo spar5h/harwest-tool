abde50cda96b18fcb16a096a890389e8076de257
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-08-28 02:16:00 +0530

    Add solution for problem `C - Rectangles` Link: https://codeforces.com/contest/1028/problem/C Tags: geometry, implementation, sortings, *1600 Ref: https://codeforces.com/contest/1028/submission/42185090

---
+++ codeforces/1028/C.java
@@ -0,0 +1,274 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+ 
+/* spar5h */
+
+public class cf3 implements Runnable{    
+	
+	static class pair {
+		
+		int i, w;
+		
+		public pair(int i, int w) {
+			this.i = i; this.w = w;
+		}
+	}
+	
+	static class comp implements Comparator<pair> {
+		
+		public int compare(pair x, pair y) {
+			
+			if(x.w < y.w)
+				return -1;
+			
+			if(x.w > y.w)
+				return 1;
+			
+			return 0;
+		}
+	}
+	
+	static class comp2 implements Comparator<pair> {
+		
+		public int compare(pair x, pair y) {
+			
+			if(x.w > y.w)
+				return -1;
+			
+			if(x.w < y.w)
+				return 1;
+			
+			return 0;
+		}
+	}
+	
+	public void run() {
+	    
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		ArrayList<pair>[] list = new ArrayList[4];
+		
+		for(int i = 0; i < 4; i++)
+			list[i] = new ArrayList<pair>();
+		
+		int n = s.nextInt();
+		
+		for(int i = 0; i < n; i++) 	
+			for(int j = 0; j < 4; j++)
+				list[j].add(new pair(i, s.nextInt()));
+		
+		for(int i = 0; i < 2; i++)
+			Collections.sort(list[i], new comp2());
+		
+		for(int i = 2; i < 4; i++)
+			Collections.sort(list[i], new comp());
+		
+		for(int i = 0; i < 4; i++) {
+			
+			int[] val = new int[4];
+			
+			int index = list[i].get(0).i;
+			
+			for(int j = 0; j < 4; j++) {
+				
+				if(index == list[j].get(0).i)
+					val[j] = list[j].get(1).w;
+				else
+					val[j] = list[j].get(0).w;
+			}
+			
+			if(val[0] <= val[2] && val[1] <= val[3]) {
+				w.println(val[0] + " " + val[1]); break;
+			}
+		}
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	}
+    
+	public static void main(String args[]) throws Exception
+	{
+		new Thread(null, new cf3(),"cf3",1<<26).start();
+	}
+}   
+
