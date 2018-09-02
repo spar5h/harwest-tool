23871fd52d56d7b96d1d62014b16178c93684b55
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-09-02 20:41:00 +0530

    Add solution for problem `D - Valid BFS?` Link: https://codeforces.com/contest/1037/problem/D Tags: dfs and similar, graphs, shortest paths, trees, *1700 Ref: https://codeforces.com/contest/1037/submission/42380517

---
+++ codeforces/1037/D.java
@@ -0,0 +1,250 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf4 implements Runnable{    
+
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		HashSet<Integer>[] adj = new HashSet[n + 1];
+		
+		for(int i = 1; i <= n; i++)
+			adj[i] = new HashSet<Integer>();
+		
+		for(int i = 0; i < n - 1; i++) {
+			
+			int u = s.nextInt(), v = s.nextInt();
+			adj[u].add(v); adj[v].add(u);
+		}
+		
+		int[] a = new int[n + 1];
+		
+		for(int i = 1; i <= n; i++)
+			a[i] = s.nextInt();
+		
+		int[] vis = new int[n + 1];
+		
+		Queue<Integer> q = new LinkedList<Integer>(); q.add(1); vis[1] = 1; adj[1].add(0); vis[0] = 1;
+		
+		boolean res = true;
+		
+		int j = 2;
+		
+		outer:
+		while(!q.isEmpty()) {
+			
+			int x = q.poll();
+			
+			for(int i = 0; i < adj[x].size() - 1; i++) {
+				
+				if(vis[a[j + i]] == 0 && adj[x].contains(a[j + i])) {
+					vis[a[j + i]] = 1; q.add(a[j + i]); 
+				}
+				else {
+					res = false; break outer;
+				}
+			}
+			
+			j += adj[x].size() - 1;
+		}
+		
+		if(a[1] == 1 && res)
+			w.println("Yes");
+		else
+			w.println("No");
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+        try
+        {
+            str = br.readLine();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return str;
+	}
+	public int nextInt()
+	{
+		int c = read();
+		
+		while(isSpaceChar(c)) 
+			c = read();
+		
+		int sgn = 1;
+		
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		
+		int res = 0;
+		do 
+		{
+			if(c<'0'||c>'9') 
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c)); 
+		
+		return res * sgn;
+	}
+	
+	public long nextLong() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		long res = 0;
+		
+		do 
+		{
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		while (!isSpaceChar(c));
+			return res * sgn;
+	}
+	
+	public double nextDouble() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') 
+		{
+			sgn = -1;
+			c = read();
+		}
+		double res = 0;
+		while (!isSpaceChar(c) && c != '.') 
+		{
+			if (c == 'e' || c == 'E')
+				return res * Math.pow(10, nextInt());
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		}
+		if (c == '.') 
+		{
+			c = read();
+			double m = 1;
+			while (!isSpaceChar(c)) 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				m /= 10;
+				res += (c - '0') * m;
+				c = read();
+			}
+		}
+		return res * sgn;
+	}
+	
+	public String readString() 
+	{
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do 
+		{
+			res.appendCodePoint(c);
+			c = read();
+		} 
+		while (!isSpaceChar(c));
+		
+		return res.toString();
+	}
+ 
+	public boolean isSpaceChar(int c) 
+	{
+		if (filter != null)
+			return filter.isSpaceChar(c);
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+ 
+	public String next() 
+	{
+		return readString();
+	}
+	
+	public interface SpaceCharFilter 
+	{
+		public boolean isSpaceChar(int ch);
+	}
+}
+
+public static void main(String args[]) throws Exception
+{
+	new Thread(null, new cf4(),"cf4",1<<26).start();
+	}
+}   
