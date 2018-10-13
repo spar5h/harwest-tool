35606820c8250c8d2e03d54d2cdfaa7ce2c5619f
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-10-13 22:59:00 +0530

    Add solution for problem `B - Vasya and Good Sequences` Link: https://codeforces.com/contest/1053/problem/B Tags: data structures, divide and conquer, dp, *2000 Ref: https://codeforces.com/contest/1053/submission/44267796

---
+++ codeforces/1053/B.java
@@ -0,0 +1,274 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf1 implements Runnable{    
+
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		int[] count = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			
+			long x = s.nextLong();
+			
+			while(x > 0) {
+				count[i] += x % 2; x /= 2;
+			}
+		}
+		
+		int[] pre = new int[n];
+		int[][] dp = new int[n][2];
+		
+		pre[0] = count[0] % 2;
+		
+		for(int i = 1; i < n; i++)
+			pre[i] = (pre[i - 1] + count[i]) % 2;
+		
+		dp[0][pre[0] % 2] = 1;
+		
+		for(int i = 1; i < n; i++) {
+			
+			dp[i][0] = dp[i - 1][0];
+			dp[i][1] = dp[i - 1][1];
+			
+			dp[i][pre[i] % 2]++;
+		}
+		
+		long res = 0;
+		
+		for(int i = 0; i < n; i++) {
+			
+			int sum = count[i];
+			int max = count[i];
+			
+			for(int j = 1; j < 64 && i + j < n; j++) {
+				
+				sum += count[i + j];
+				max = max(count[i + j], max);
+				
+				if(sum % 2 == 0 && sum - max >= max)
+					res++;
+			}
+			
+			if(i + 64 < n) {
+				
+				int x = 0;
+				
+				if(i - 1 >= 0)
+					x = pre[i - 1];
+				
+				res += dp[n - 1][x] - dp[i + 64 - 1][x]; 
+			}
+		}
+		
+		w.println(res);
+		
+		w.close();
+	}
+
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf1(),"cf1",1<<26).start();
+	}
+}   
