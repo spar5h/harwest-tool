d49a3ee6fc160fe3945533518f6c1940c683f2cf
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-10-20 15:54:00 +0530

    Add solution for problem `B - Minimum and Maximum` Link: https://codeforces.com/contest/730/problem/B Tags: constructive algorithms, interactive, *1800 Ref: https://codeforces.com/contest/730/submission/44588743

---
+++ codeforces/730/B.java
@@ -0,0 +1,293 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf1 implements Runnable{    
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+	
+		int t = s.nextInt();
+		
+		while(t-- > 0) {
+			
+			int n = s.nextInt();
+			
+			ArrayList<Integer> max = new ArrayList<Integer>();
+			
+			ArrayList<Integer> min = new ArrayList<Integer>();
+			
+			if(n % 2 == 1) {
+				min.add(n); max.add(n);
+			}	
+			
+			for(int i = 1; i + 1 <= n; i+=2) {
+				
+				System.out.println("? " + i + " " + (i + 1));
+				System.out.flush();
+				
+				char ch = s.next().charAt(0);
+				
+				if(ch == '>') {
+					max.add(i); min.add(i + 1);
+				}
+				else {
+					max.add(i + 1); min.add(i);
+				}
+			}
+			
+			while(max.size() > 1) {
+				
+				ArrayList<Integer> temp = new ArrayList<Integer>();
+				
+				if(max.size() % 2 == 1)
+					temp.add(max.get(max.size() - 1));
+				
+				for(int i = 0; i + 1 < max.size(); i+=2) {
+					
+					System.out.println("? " + max.get(i) + " " + max.get(i + 1));
+					
+					char ch = s.next().charAt(0);
+					
+					if(ch == '<') 
+						temp.add(max.get(i + 1)); 
+					
+					else 
+						temp.add(max.get(i));					
+				}
+				
+				max = temp;
+			}
+			
+			while(min.size() > 1) {
+				
+				ArrayList<Integer> temp = new ArrayList<Integer>();
+				
+				if(min.size() % 2 == 1)
+					temp.add(min.get(min.size() - 1));
+				
+				for(int i = 0; i + 1 < min.size(); i+=2) {
+					
+					System.out.println("? " + min.get(i) + " " + min.get(i + 1));
+					
+					char ch = s.next().charAt(0);
+					
+					if(ch == '>') 
+						temp.add(min.get(i + 1)); 
+					
+					else 
+						temp.add(min.get(i));					
+				}
+				
+				min = temp;
+			}
+			
+			System.out.println("! " + min.get(0) + " " + max.get(0));
+			System.out.flush();
+		}
+		
+		w.close();
+	}
+
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf1(),"cf1",1<<26).start();
+	}
+}   
