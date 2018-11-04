b2ddb17761fa44d58070ca52e54f66bea5fa3849
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-11-05 00:00:00 +0530

    Add solution for problem `A - The Tower is Going Home` Link: https://codeforces.com/contest/1074/problem/A Tags: sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1074/submission/45291965

---
+++ codeforces/1074/A.java
@@ -0,0 +1,265 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf1 implements Runnable{    
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+
+		int n = s.nextInt(), m = s.nextInt();
+		
+		ArrayList<Integer> v = new ArrayList<Integer>();
+		
+		for(int i = 0; i < n; i++)
+			v.add(s.nextInt());
+		
+		v.add((int)1e9);
+		
+		n++;
+		
+		Collections.sort(v);
+		
+		int[] freq = new int[n];
+		
+		for(int i = 0; i < m; i++) {
+			
+			int x1 = s.nextInt(), x2 = s.nextInt(), y = s.nextInt();
+			
+			if(x1 != 1)
+				continue;
+			
+			int left = 0, right = v.size() - 1;
+			int ans = -1;
+			
+			while(left <= right) {
+				
+				int mid = (left + right) / 2;
+				
+				if(x2 >= v.get(mid)) {
+					left = mid + 1; ans = mid;
+				}
+				else
+					right = mid - 1;
+			}
+			
+			if(ans != -1)
+				freq[ans]++;
+		}
+		
+		for(int i = n - 2; i >= 0; i--)
+			freq[i] += freq[i + 1];
+		
+		long res = Long.MAX_VALUE;
+		
+		for(int i = 0; i < n; i++) {
+			res = min(res, freq[i] + i);
+		}
+		
+		w.println(res);
+		
+		w.close();
+	}
+
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf1(),"cf1",1<<26).start();
+	}
+}   
