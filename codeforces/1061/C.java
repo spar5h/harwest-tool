c1978d17f36eb8028102b530a85854fa39ff61ae
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-11-22 22:00:00 +0530

    Add solution for problem `C - Multiplicity` Link: https://codeforces.com/contest/1061/problem/C Tags: data structures, dp, implementation, math, number theory, *1700 Ref: https://codeforces.com/contest/1061/submission/46076048

---
+++ codeforces/1061/C.java
@@ -0,0 +1,256 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf3 implements Runnable {    
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		int[] a = new int[n + 1];
+		
+		for(int i = 1; i <= n; i++)
+			a[i] = s.nextInt();
+		
+		//handle 1
+		
+		long mod = (long)1e9 + 7;
+		long[] dp = new long[n + 1]; dp[0] = 1;
+		
+		HashMap<Integer, Long> hm = new HashMap<Integer, Long>();
+		
+		for(int i = 1; i <= n; i++) {
+			
+			hm.clear();
+			
+			for(int j = 1; (long)j * j <= a[i]; j++) {
+				
+				if(a[i] % j != 0)
+					continue;
+				
+				if(j <= n)
+					hm.put(j, dp[j - 1]);
+				
+				if(a[i] / j <= n && a[i] / j != j)
+					hm.put(a[i] / j, dp[a[i] / j - 1]);
+			}
+			
+			for(Map.Entry<Integer, Long> e : hm.entrySet()) {
+				dp[e.getKey()] = (dp[e.getKey()] + e.getValue()) % mod;
+			}
+		}
+		
+		long res = 0;
+		
+		for(int i = 1; i <= n; i++)
+			res = (res + dp[i]) % mod;
+		
+		w.println(res);
+		
+		w.close();
+	}
+
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf3(),"cf3",1<<26).start();
+	}
+}   
