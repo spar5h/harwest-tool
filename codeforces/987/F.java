67209076621984ccbbac1093dff0a2faf65b21c7
Sparsh Sanchorawala <sparshno.1@gmail.com>
2018-12-21 22:46:00 +0530

    Add solution for problem `F - AND Graph` Link: https://codeforces.com/contest/987/problem/F Tags: bitmasks, dfs and similar, graphs, *2500 Ref: https://codeforces.com/contest/987/submission/47340848

--- codeforces/987/F.java
+++ codeforces/987/F.java
@@ -24,7 +24,7 @@ public class cf6 implements Runnable{
 	
 	public void run() {
 	    
-		FastReader s = new FastReader();
+		InputReader s = new InputReader(System.in);
 		PrintWriter w = new PrintWriter(System.out);
 		
 		n = s.nextInt();
@@ -55,55 +55,181 @@ public class cf6 implements Runnable{
 		w.close();
 	}
 	
-	static class FastReader {
+	static class InputReader {
 		
-		BufferedReader br;
-		StringTokenizer st;
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
 		
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
 		}
 		
-		String next() {
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
 			
-			while(st == null || !st.hasMoreTokens()) {
-				
-				try {
-					st = new StringTokenizer(br.readLine());
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
 				}
 				
-				catch (IOException e) {
-					e.printStackTrace();
-				}
+				if(numChars <= 0)				
+					return -1;
 			}
-			
-			return st.nextToken();
+			return buf[curChar++];
 		}
-		
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-		
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-		
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-		
-		String nextLine() {
-			
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
 			
-			try {
-				str = br.readLine();
-			}
-			catch(IOException e) {
-				e.printStackTrace();
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
 			}
 			
-			return str;
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
 		}
 	}
     
