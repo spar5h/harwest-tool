9a57824e887c8ad2c78de085d84720c52384de4b
Sparsh Sanchorawala <sparshno.1@gmail.com>
2019-01-01 16:31:00 +0530

    Add solution for problem `B - Vova and Trophies` Link: https://codeforces.com/contest/1082/problem/B Tags: greedy, *1600 Ref: https://codeforces.com/contest/1082/submission/47810590

---
+++ codeforces/1082/B.java
@@ -0,0 +1,264 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.Math.*;
+
+public class cf2 implements Runnable {   
+	
+	public void run() {
+			
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt();
+		
+		char[] a = s.next().toCharArray();
+		
+		int g = 0;
+		
+		for(int i = 0; i < n; i++)
+			if(a[i] == 'G')
+				g++;
+		
+		int ans = 0;
+		
+		int j = 0;
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		while(j < n) {
+			
+			char key = a[j];
+			int c = 0;
+			
+			while(j < n && a[j] == key) {
+				j++; c++;
+			}
+			
+			if(key == 'G') {
+				
+				if(g == c)
+					ans = max(c, ans);
+				else
+					ans = max(c + 1, ans);
+				
+				list.add(c);
+			}
+			
+			else {
+			
+				list.add(-c);
+			}
+		}
+		
+		for(int i = 0; i < list.size() - 2; i++) {
+			
+			if(list.get(i) < 0 || list.get(i + 1) < -1)
+				continue;
+			
+			if(list.get(i) + list.get(i + 2) == g)
+				ans = max(list.get(i) + list.get(i + 2), ans);
+			else
+				ans = max(list.get(i) + list.get(i + 2) + 1, ans);
+		}
+		
+		w.println(ans);
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf2(),"cf2",1<<26).start();
+	}
+}   
