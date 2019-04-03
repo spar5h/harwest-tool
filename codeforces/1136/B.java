d98007d8d7865720f20ae9653bcb03bd839db06f
Sparsh Sanchorawala <sparshno.1@gmail.com>
2019-04-03 22:30:00 +0530

    Add solution for problem `B - Nastya Is Playing Computer Games` Link: https://codeforces.com/contest/1136/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/1136/submission/52280964

---
+++ codeforces/1136/B.java
@@ -0,0 +1,272 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+import static java.lang.Math.*;
+
+/* spar5h */
+
+public class cf2 implements Runnable {   
+	
+	public void run() {
+		
+		InputReader s = new InputReader(System.in);
+		PrintWriter w = new PrintWriter(System.out);
+		
+		int n = s.nextInt(), k = s.nextInt() - 1;
+		
+		int res = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j = 0; j < n; j++) {
+				
+				if(i == j)
+					continue;
+				
+				int can = 0;
+				
+				can += abs(k - i);
+				can++;
+				can++;
+				
+				can += abs(i - j);
+				can+=2;
+				can++;
+				
+				if(i < j) {
+					
+					if(i > 0 && j < n - 1) 
+						can += min(n - 1 - j, j) + n - 1;
+					
+					else if(i > 0) 
+						can += j;
+					
+					else if(j < n - 1) 
+						can += min(j - i - 1, n - 1 - j) + n - 1 - (j - (j - i - 1));
+					
+					else
+						can += j - i - 1;
+				}
+				
+				else {
+					
+					if(i < n - 1 && j > 0) 
+						can += min(n - 1 - j, j) + n - 1;
+					
+					else if(j > 0) 
+						can += min(i - j - 1, j) + i - 1;
+					
+					else if(i < n - 1) 
+						can += n - 1 - j;
+					
+					else
+						can += i - j - 1;
+				}
+				
+				can += 2 * (n - 2);
+				
+				res = min(can, res);
+			}
+		}
+		
+		w.println(res);
+		
+		w.close();
+	}
+	
+	static class InputReader {
+		
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars) {
+				
+				curChar = 0;
+				
+				try {
+					numChars = stream.read(buf);
+				}
+				catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			
+			return buf[curChar++];
+		}
+		 
+		public String nextLine() {
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+	        
+			try {
+	            str = br.readLine();
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+	        return str;
+		}
+		
+		public int nextInt() {
+		
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			
+			do {
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			long res = 0;
+			
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			
+			double res = 0;
+			
+			while (!isSpaceChar(c) && c != '.') {
+				
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			
+			if (c == '.') {
+				
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			
+			return res * sgn;
+		}
+		
+		public String readString() {
+			
+			int c = read();
+			
+			while (isSpaceChar(c))
+				c = read();
+			
+			StringBuilder res = new StringBuilder();
+			
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) {
+			
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() {
+			return readString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	
+	}
+
+	public static void main(String args[]) throws Exception {
+		new Thread(null, new cf2(),"cf2",1<<26).start();
+	}
+}
