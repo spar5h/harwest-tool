40b3a5b8c9e9f32ff3defc85b4d8349405bc429f
Sparsh Sanchorawala <sparshno.1@gmail.com>
2019-08-11 18:09:00 +0530

    Add solution for problem `A - Hotelier` Link: https://codeforces.com/contest/1200/problem/A Tags: brute force, data structures, implementation, *800 Ref: https://codeforces.com/contest/1200/submission/58576858

---
+++ codeforces/1200/A.java
@@ -0,0 +1,145 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        AHotelier solver = new AHotelier();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class AHotelier {
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            int n = s.nextInt();
+            char[] a = s.next().toCharArray();
+            int[] res = new int[10];
+            for (int i = 0; i < n; i++) {
+                if (a[i] == 'L') {
+                    for (int j = 0; j < 10; j++) {
+                        if (res[j] == 0) {
+                            res[j] = 1;
+                            break;
+                        }
+                    }
+                } else if (a[i] == 'R') {
+                    for (int j = 9; j >= 0; j--) {
+                        if (res[j] == 0) {
+                            res[j] = 1;
+                            break;
+                        }
+                    }
+                } else {
+                    res[a[i] - '0'] = 0;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+                w.print(res[i]);
+            w.println();
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return nextString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
