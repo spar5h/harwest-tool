172feaf2dfcca935646afb250aa09ccbcf8b85aa
Sparsh Sanchorawala <sparshno.1@gmail.com>
2019-08-11 18:30:00 +0530

    Add solution for problem `C - Round Corridor` Link: https://codeforces.com/contest/1200/problem/C Tags: math, number theory, *1400 Ref: https://codeforces.com/contest/1200/submission/58588064

---
+++ codeforces/1200/C.java
@@ -0,0 +1,150 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        CRoundCorridor solver = new CRoundCorridor();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class CRoundCorridor {
+        long gcd(long a, long b) {
+            if (b == 0)
+                return a;
+            return (gcd(b, a % b));
+        }
+
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            long n = s.nextLong(), m = s.nextLong();
+            long count = gcd(n, m);
+            int q = s.nextInt();
+            while (q-- > 0) {
+                int type = s.nextInt();
+                long a = s.nextLong() - 1;
+                if (type == 1) {
+                    a = a / (n / count);
+                } else
+                    a = a / (m / count);
+                type = s.nextInt();
+                long b = s.nextLong() - 1;
+                if (type == 1) {
+                    b = b / (n / count);
+                } else
+                    b = b / (m / count);
+                if (a == b)
+                    w.println("YES");
+                else
+                    w.println("NO");
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
