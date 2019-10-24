bb623f0c8a560ede96bc01c1e760eba45191a69f
Sparsh Sanchorawala <sparshno.1@gmail.com>
2019-10-24 20:40:00 +0530

    Add solution for problem `A - Broken Keyboard` Link: https://codeforces.com/contest/1251/problem/A Tags: brute force, strings, two pointers, *1000 Ref: https://codeforces.com/contest/1251/submission/63304571

---
+++ codeforces/1251/A.java
@@ -0,0 +1,141 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        ABrokenKeyboard solver = new ABrokenKeyboard();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class ABrokenKeyboard {
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            int t = s.nextInt();
+            while (t-- > 0) {
+                int[] check = new int[26];
+                char[] a = s.next().toCharArray();
+                int n = a.length;
+                int i = 0;
+                while (i < n) {
+                    int j = i;
+                    while (j < n && a[j] == a[i]) {
+                        j++;
+                    }
+                    if ((j - i) % 2 == 1) {
+                        check[a[i] - 'a'] = 1;
+                    }
+                    i = j;
+                }
+                for (int j = 0; j < 26; j++)
+                    if (check[j] == 1)
+                        w.print((char) ('a' + j));
+                w.println();
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                if (Character.isValidCodePoint(c)) {
+                    res.appendCodePoint(c);
+                }
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return nextString();
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
