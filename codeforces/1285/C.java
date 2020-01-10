18ce99c21b2bcd58f67c4860a75a0d2cace2a40c
Sparsh Sanchorawala <sparshno.1@gmail.com>
2020-01-10 20:06:00 +0530

    Add solution for problem `C - Fadi and LCM` Link: https://codeforces.com/contest/1285/problem/C Tags: brute force, math, number theory, *1400 Ref: https://codeforces.com/contest/1285/submission/68516722

---
+++ codeforces/1285/C.java
@@ -0,0 +1,122 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        CFadiAndLCM solver = new CFadiAndLCM();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class CFadiAndLCM {
+        long gcd(long a, long b) {
+            if (b == 0)
+                return a;
+            return (gcd(b, a % b));
+        }
+
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            long x = s.nextLong();
+            long res = x;
+            long i1 = 1, i2 = x;
+            for (long i = 1; i * i <= x; i++) {
+                if (x % i != 0)
+                    continue;
+                if (gcd(i, x / i) != 1)
+                    continue;
+                if (Math.max(i, x / i) < res) {
+                    res = Math.max(i, x / i);
+                    i1 = i;
+                    i2 = x / i;
+                }
+            }
+            w.println(i1 + " " + i2);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
