8069dbdb5826006eaa6dc536f9d112fbf06b953b
Sparsh Sanchorawala <sparshno.1@gmail.com>
2020-04-07 00:30:00 +0530

    Add solution for problem `D - Walk on Matrix` Link: https://codeforces.com/contest/1332/problem/D Tags: bitmasks, constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1332/submission/75688560

---
+++ codeforces/1332/D.java
@@ -0,0 +1,115 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        DWalkOnMatrix solver = new DWalkOnMatrix();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class DWalkOnMatrix {
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            int k = s.nextInt();
+            int[][] a = new int[3][2];
+            a[0][0] = (1 << 18) - 1;
+            a[0][1] = (1 << 17) - 1;
+            a[1][0] = 1 << 17;
+            a[1][1] = (1 << 18) - 1;
+            a[2][0] = 0;
+            a[2][1] = k;
+            w.println(3 + " " + 2);
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 2; j++)
+                    w.print(a[i][j] + " ");
+                w.println();
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
