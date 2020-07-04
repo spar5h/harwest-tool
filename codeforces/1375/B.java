ed43a0179dc8a89a71ab8b552fba27e447f612c9
Sparsh Sanchorawala <sparshno.1@gmail.com>
2020-07-04 20:32:00 +0530

    Add solution for problem `B - Neighbor Grid` Link: https://codeforces.com/contest/1375/problem/B Tags: constructive algorithms, greedy, *1200 Ref: https://codeforces.com/contest/1375/submission/85941616

---
+++ codeforces/1375/B.java
@@ -0,0 +1,137 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        BNeighborGrid solver = new BNeighborGrid();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class BNeighborGrid {
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            int t = s.nextInt();
+            while (t-- > 0) {
+                int n = s.nextInt(), m = s.nextInt();
+                int[][] a = new int[n][m];
+                int[] di = {-1, 1, 0, 0};
+                int[] dj = {0, 0, -1, 1};
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        for (int k = 0; k < 4; k++) {
+                            if (i + di[k] >= 0 && i + di[k] < n && j + dj[k] >= 0 && j + dj[k] < m) {
+                                a[i][j]++;
+                            }
+                        }
+                    }
+                }
+                boolean check = true;
+                int[][] b = new int[n][m];
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        b[i][j] = s.nextInt();
+                        if (b[i][j] > a[i][j]) {
+                            check = false;
+                        }
+                    }
+                }
+                if (check) {
+                    w.println("YES");
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            w.print(a[i][j] + " ");
+                        }
+                        w.println();
+                    }
+                } else
+                    w.println("NO");
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
