3be2709f9212e32ec956200dca0f0448f6989ce8
Sparsh Sanchorawala <sparshno.1@gmail.com>
2020-07-24 20:53:00 +0530

    Add solution for problem `B - GameGame` Link: https://codeforces.com/contest/1383/problem/B Tags: bitmasks, constructive algorithms, dp, games, greedy, math, *1900 Ref: https://codeforces.com/contest/1383/submission/87895700

---
+++ codeforces/1383/B.java
@@ -0,0 +1,132 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Sparsh Sanchorawala
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        BGameGame solver = new BGameGame();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class BGameGame {
+        public void solve(int testNumber, InputReader s, PrintWriter w) {
+            int t = s.nextInt();
+            while (t-- > 0) {
+                int n = s.nextInt();
+                int[] a = new int[n];
+                int xor = 0;
+                for (int i = 0; i < n; i++) {
+                    a[i] = s.nextInt();
+                    xor ^= a[i];
+                }
+                if (xor == 0) {
+                    w.println("DRAW");
+                    continue;
+                }
+                int k = -1;
+                while (xor > 0) {
+                    k++;
+                    xor /= 2;
+                }
+                int[] f = new int[2];
+                for (int i = 0; i < n; i++) {
+                    f[(a[i] >> k) & 1]++;
+                }
+                if ((f[1] - 1) % 4 == 0) {
+                    w.println("WIN");
+                    continue;
+                }
+                if (f[0] % 2 == 1)
+                    w.println("WIN");
+                else
+                    w.println("LOSE");
+            }
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+}
+
